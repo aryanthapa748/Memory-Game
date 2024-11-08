dffe24918f983b815bc40f77df1295781822ced9
Aryan Thapa <aryanthapa748@gmail.com>
2024-11-08 12:24:55 -0500

    Final game logic implementation, more features and styling yet to be added

--- memory.js
+++ memory.js
@@ -71,11 +71,14 @@ const cardArray = [
 
 ]
 
-const cardsChosen = [];
+const gridDisplay = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#resultDisplay');
+
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
 
 cardArray.sort(() => 0.5 - Math.random());
-
-const gridDisplay = document.querySelector('#grid');
 
 function createBoard(){
     for (let i = 0; i < cardArray.length; i++){
@@ -93,6 +96,43 @@ createBoard();
 function flipCard(){
     const cardId = this.getAttribute('data-id');
     cardsChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId); 
     this.setAttribute('src', cardArray[cardId].img);
+    if(cardsChosen.length === 2){
+        setTimeout(checkMatch, 500)
+    } 
+
+}
+
+function checkMatch(){
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenIds[0];
+    const optionTwoId = cardsChosenIds[1];
+    if (optionOneId == optionTwoId){
+        cards[optionOneId].setAttribute('src', 'images/soldout.png');
+        cards[optionTwoId].setAttribute('src', 'images/soldout.png');
+        alert('You have clicked the same image!')
+    }
+    if (cardsChosen[0] === cardsChosen[1]){
+        alert('You found a match');
+        cards[optionOneId].setAttribute('src', 'images/soldout.png');
+        cards[optionTwoId].setAttribute('src', 'images/soldout.png');
+        cards[optionOneId].removeEventListener('click', flipCard);
+        cards[optionTwoId].removeEventListener('click', flipCard);
+        cardsWon.push(cardsChosen);
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/card.png');
+        cards[optionTwoId].setAttribute('src', 'images/card.png');
+        alert('Sorry try again');
+    }
+    cardsChosen = [];
+    cardsChosenIds =[];
+    resultDisplay.textContent = cardsWon.length
+
+    if(cardsWon.length == cardArray.length/2){
+        resultDisplay.textContent = 'Congratulations you found them all!'
+
+    }
+
 
 }
